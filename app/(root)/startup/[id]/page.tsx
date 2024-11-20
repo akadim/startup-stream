2b74ae349702668180072527d08b763abdfed4f0
Ahmed KADIM <kadim.ahmed.web@gmail.com>
2024-11-20 23:54:01 +0100

    Adding the Startup List + Filtering Startups

---
+++ app/(root)/startup/[id]/page.tsx
@@ -0,0 +1,10 @@
+const StartupDetails = async ({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) => {
+  const id = (await params).id;
+  return <h1 className="text-3xl">This is a startup number: {id}</h1>;
+};
+
+export default StartupDetails;
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["img.freepik.com", "media.istockphoto.com"],
+  },
 };
 
 export default nextConfig;
